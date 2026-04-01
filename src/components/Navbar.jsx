3575627486dfd51e63690ac721dbbbdc98538616
akashzone <akashnadar.dev@gmail.com>
2026-04-01 20:30:18 +0530

    setup react & build navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return <>
+        <div className="nav-title">
+            Smart <span>Todo</span>
+        </div>
+    </>
+}
+
+export default Navbar
