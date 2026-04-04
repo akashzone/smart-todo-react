f7d3b24396971d5b372efe4e902a880640449fa2
akashzone <akashnadar.dev@gmail.com>
2026-04-04 07:08:14 +0530

    style navbar & add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className='footer'>
+            Copyright &copy; 2023 Smart Todo. All rights reserved.
+        </div>
+    )
+}
+
+export default Footer
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
 
+import {useState} from "react"
+
+import './Navbar.css'
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false)
+
     return <>
-        <div className="nav-title">
+    <nav className="navbar">
+        <div className="navTitle">
             Smart <span>Todo</span>
         </div>
+        <div className="toggle">
+            <button className="toggleBtn"
+            onClick={()=>{
+                setToggle(!toggle);
+            }}
+            >{toggle ? "Light" : "Dark"}</button>
+        </div>
+    </nav>
     </>
 }
 
